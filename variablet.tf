3255090ac31277d90a58643da08be2a299eec375
savitawailthare <savitawailthare8@gmail.com>
2023-05-18 19:38:53 +0530

    my change2

---
+++ variablet.tf
@@ -0,0 +1,35 @@
+variable "region"{
+    type=string
+    default="ap-south-1"
+}
+variable "ami"{
+    type=string
+    default="ami-08333bccc35d71140"
+}
+
+variable "pub_ip"{
+    type=bool
+}
+
+variable "key_pair"  {
+    type=string
+default="ohio-new"
+}
+
+variable "role"  {
+    type=string
+}
+
+variable "tag"  {
+    type=map
+default={
+    name="demo-tf-server"
+    env="UAT"
+    Dept="operation"
+}
+}
+
+variable "username" {
+  type = list(string)
+  default = ["tucker","annie","josh","sagar"]
+}
